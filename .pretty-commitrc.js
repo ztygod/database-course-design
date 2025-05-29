8ea609d577861241b77a2224e025083cdd922109
TianYi <2739022972@qq.com>
2025-05-29 09:33:22 +0800

    feat: 添加登录功能

---
+++ .pretty-commitrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+      'type-enum': ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    },
+    messages: {
+      success: '✅ 提交成功啦！🎉',
+      invalid: '❌ 提交信息不规范，请使用如：feat: 添加功能',
+    },
+    emoji: true, // 添加 emoji 图标
+  };
+  
